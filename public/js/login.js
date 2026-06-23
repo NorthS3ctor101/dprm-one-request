b8a9818e38f3bc8ac837686cb9dc6e126518996f
NorthS3ctor101 <90097679+NorthS3ctor101@users.noreply.github.com>
2026-06-23 15:20:54 +0800

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -9,7 +9,7 @@ function login() {
   if (!code) {
     showNotification("Security code is required to proceed.");
     return;
-}
+  }
 
   loginBtn.disabled = true;
   loginBtn.innerHTML = `<div class="animate-spin inline-block rounded-full h-4 w-4 border-2 border-white border-t-transparent mr-2 align-middle"></div>Logging in...`;
