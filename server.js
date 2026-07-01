690ce344deafc04ab65a61aa469b82a8b48cc45c
NorthS3ctor101 <90097679+NorthS3ctor101@users.noreply.github.com>
2026-07-01 21:49:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.use('/api/proxy', async (req, res) => {
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
 app.get('/admin/dashboard', (req, res) => res.sendFile(path.join(__dirname, 'public', 'tracker.html')));
+app.get('/queuing', (req, res) => res.sendFile(path.join(__dirname, 'public', 'queuing.html')));
 
 app.use((req, res) => {
   res.status(404).send('<h1>404 Resource Not Found</h1>');
